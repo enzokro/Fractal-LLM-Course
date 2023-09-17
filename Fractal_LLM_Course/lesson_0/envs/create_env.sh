e30f26b5353658ead41534b50c496710b00a9ee5
Chris Kroenke <cck@MacBook-Pro-de-Chris.local>
2023-09-17 11:28:33 -0400

    update after brick

--- Fractal_LLM_Course/lesson_0/envs/create_env.sh
+++ Fractal_LLM_Course/lesson_0/envs/create_env.sh
@@ -6,8 +6,8 @@ mamba env create -f environment.yml
 # activate the environment
 mamba activate llm_base
 
-# install the python packages, after activating the env
-python -m pip install -r requirements.txt  
-
 # install the pytorch library
 python -m pip install -r reqs_torch_cpu.txt
+
+# install the python packages, after activating the env
+python -m pip install -r requirements.txt  
